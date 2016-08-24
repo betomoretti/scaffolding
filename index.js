629fd1c8dbb4e2c6f6aaaa196b7d135d3101d553
Mariano Moretti <mariano.moretti@nan-labs.com>
2016-08-24 14:43:05 -0300

    Folder with files inside

--- index.js
+++ index.js
@@ -1,39 +1,22 @@
 let fs = require('fs'),
   inquirer = require('inquirer'),
   Q = require('q'),
-  questions = require('./questions');
+  questions = require('./questions'),
+  writeFile = Q.denodeify(fs.writeFile),
+  mkdir = Q.denodeify(fs.mkdir);
 
-
-function createFile(name, data) {
-  return new Promise(function(resolve, reject) {
-    fs.writeFile(name, data, 'utf-8', function(err) {
-      if (err) reject(err);
-      else resolve(data);
-    });
-  });
-}
-
-function createFolder(dir) {
-
-  if (!fs.statSync(dir)){
-    return new Promise(function(resolve, reject) {
-      fs.mkdir(dir, function(err) {
-        if (err) reject(err);
-        else resolve(dir);
-      });
-    });
-  }
-}
-
-inquirer
+let promise = inquirer
   .prompt(questions)
-  .then((answers) => Q.all([answers, createFolder(answers.name)]))
-  .then((answers) =>
+  .then((answers) => Q.all([answers, mkdir(answers.name)]));
+
+Q(promise)
+  .spread((answers) =>
     Q.all([
-      createFile(`${answers.name}/${answers.name}Controller.js`, `${answers.name}/${answers.name}Controller.js`),
-      createFile(`${answers.name}/${answers.name}Service.js`, `${answers.name}/${answers.name}Service.js`),
-      createFile(`${answers.name}/${answers.name}Validator.js`, `${answers.name}/${answers.name}Validator.js`),
-      createFile(`${answers.name}/${answers.name}Model.js`, `${answers.name}/${answers.name}Model.js`),
+      writeFile(`${answers.name}/${answers.name}Controller.js`, `${answers.name}/${answers.name}Controller.js`),
+      writeFile(`${answers.name}/${answers.name}Service.js`, `${answers.name}/${answers.name}Service.js`),
+      writeFile(`${answers.name}/${answers.name}Validator.js`, `${answers.name}/${answers.name}Validator.js`),
+      writeFile(`${answers.name}/${answers.name}Model.js`, `${answers.name}/${answers.name}Model.js`),
       ])
   )
-  .then((results) => console.log('oks'));
+  .then((results) => console.log('oks'))
+  .catch(console.log);
