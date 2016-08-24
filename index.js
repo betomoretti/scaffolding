7be6c96afa48b080e360131297e2a64e73d62e7b
Mariano Moretti <mariano.moretti@nan-labs.com>
2016-08-24 12:07:08 -0300

    WIP Folder cretion

--- index.js
+++ index.js
@@ -13,24 +13,27 @@ function createFile(name, data) {
   });
 }
 
-// function createFolder(name, data) {
-//   return new Promise(function(resolve, reject) {
-//     fs.writeFile(name, data, 'utf-8', function(err) {
-//       if (err) reject(err);
-//       else resolve(data);
-//     });
-//   });
-// }
+function createFolder(dir) {
+
+  if (!fs.statSync(dir)){
+    return new Promise(function(resolve, reject) {
+      fs.mkdir(dir, function(err) {
+        if (err) reject(err);
+        else resolve(dir);
+      });
+    });
+  }
+}
 
 inquirer
   .prompt(questions)
-  // .then((answers) => [answers, createFolder(answers.name)])
+  .then((answers) => Q.all([answers, createFolder(answers.name)]))
   .then((answers) =>
     Q.all([
-      createFile(`${answers.name}Controller.js`, `${answers.name}Controller.js`),
-      createFile(`${answers.name}Service.js`, `${answers.name}Service.js`),
-      createFile(`${answers.name}Validator.js`, `${answers.name}Validator.js`),
-      createFile(`${answers.name}Model.js`, `${answers.name}Model.js`),
+      createFile(`${answers.name}/${answers.name}Controller.js`, `${answers.name}/${answers.name}Controller.js`),
+      createFile(`${answers.name}/${answers.name}Service.js`, `${answers.name}/${answers.name}Service.js`),
+      createFile(`${answers.name}/${answers.name}Validator.js`, `${answers.name}/${answers.name}Validator.js`),
+      createFile(`${answers.name}/${answers.name}Model.js`, `${answers.name}/${answers.name}Model.js`),
       ])
   )
   .then((results) => console.log('oks'));
