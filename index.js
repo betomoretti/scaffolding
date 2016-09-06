65a1d4f39cf9898e84b845ea27356eecc58a2795
Mariano Moretti <mariano.moretti@nan-labs.com>
2016-09-06 16:35:52 -0300

    WIP adding attributes to model

--- index.js
+++ index.js
@@ -5,11 +5,24 @@ let fs = require('fs'),
   _ = require('lodash'),
   writeFile = Q.denodeify(fs.writeFile),
   mkdir = Q.denodeify(fs.mkdir),
-  TemplateHelper = require('./TemplateHelper');
+  TemplateHelper = require('./TemplateHelper'),
+  attributes = [];
+
+let ask = () => {
+  inquirer.prompt(questions.attributes).then((answers) => {
+    attributes.push(answers.attribute);
+    if (answers.askAgain) {
+      ask();
+    } else {
+      return attributes;
+    }
+  });
+}
 
 let promise = inquirer
-  .prompt(questions)
-  .then((answers) => Q.all([answers, mkdir(answers.name)]));
+  .prompt(questions.name)
+  .then((answers) => Q.all([answers.name, ask()]))
+  .spread((name, attributes) => Q.all([{'name': name, 'attr': attributes}, mkdir(name)]));
 
 Q(promise)
   .spread((answers) =>
@@ -19,7 +32,9 @@ Q(promise)
       writeFile(`${answers.name}/${_.capitalize(answers.name)}Validator.js`, TemplateHelper.controller(answers)),
       writeFile(`${answers.name}/${_.capitalize(answers.name)}Model.js`, TemplateHelper.controller(answers)),
       writeFile(`${answers.name}/index.js`, TemplateHelper.controller(answers)),
-      ])
-  )
+    ])
+)
   .then((results) => console.log('oks'))
   .catch(console.log);
+
+
